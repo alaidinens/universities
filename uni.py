5891b7da2257f681f96d13da4c91e1b876eb3ca8
alaidinens <alexey.laidinen@gmail.com>
2023-02-17 10:45:33 +0200

    Update uni.py

--- uni.py
+++ uni.py
@@ -2,48 +2,38 @@ from tkinter import *
 import requests
 import json
 from datetime import datetime
-from collections import defaultdict
+from collections import OrderedDict
  
 root =Tk()
 root.geometry("400x400") #size of the window by default
 root.resizable(0,0) #to make the window size fixed
 #title of our window
-root.title("Weather App - AskPython.com")
+root.title("University list")
 
-city_value = StringVar()
+country_value = StringVar()
  
 def showUni():
     #Enter you api key, copies from the OpenWeatherMap dashboard
     api_key = "c774716a7b3ab97e1151d8304e6b0bf9"  #sample API
  
     # Get city name from user from the input field (later in the code)
-    city_name=city_value.get()
+    country_name=country_value.get()
  
     # API url
-    uni_url = 'http://universities.hipolabs.com/search?country=' + city_name
+    uni_url = 'http://universities.hipolabs.com/search?country=' + country_name
  
     # Get the response from fetched url
     response = requests.get(uni_url)
  
     # changing response from json to python readable 
-    uni_info = response.json()
+    uni_info = str(response.json())
+    print(uni_info)
     
-    with  open(uni_info) as input_file:
-        old_data= json.load(input_file)
-    
-    d=defaultdict(dict)
-    for item in old_data:
-        d[item['name']].update(item)
-    
-    with open("output.json", "w") as output_file:
-        json.dump(list(d.values()), output_file, indent=4)
-    
-
-    tfield.delete("1.0", "end")   #to clear the text field for every new output                                   #converting default kelvin value to Celcius
-         
-    
- 
-    tfield.insert(INSERT, output_file)   #to insert or send value in our Text Field to display output
+    split= uni_info.split("name")
+    split_name= dict(split)  
+    split_name['name'] = sorted(uni_info['domain'], key=lambda x : x['name'], reverse=True)
+    print(split_name)
+    tfield.insert(INSERT, split_name)   #to insert or send value in our Text Field to display output
  
  
  
@@ -52,7 +42,7 @@ def showUni():
  
 city_head= Label(root, text = 'Enter City Name', font = 'Arial 12 bold').pack(pady=10) #to generate label heading
  
-inp_city = Entry(root, textvariable = city_value,  width = 24, font='Arial 14 bold').pack()
+inp_city = Entry(root, textvariable = country_value,  width = 24, font='Arial 14 bold').pack()
 
  
 Button(root, command = showUni, text = "Check University", font="Arial 10", bg='lightblue', fg='black', activebackground="teal", padx=5, pady=5 ).pack(pady= 20)
